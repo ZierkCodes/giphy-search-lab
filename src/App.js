f770b3ea27925de487c8b42fc6ae091d5e925433
ZierkCodes <zierk.gage@gmail.com>
2021-08-12 12:23:01 -0400

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {  
+    baseURL: 'https://api.giphy.com/v1/gifs/search?',
+    apikey: 'api_key=' + process.env.REACT_APP_API_KEY,
+    query: '&q=',
+    gifName: '',
+    searchURL: '',
+    gif: [],
+  }
+handleSubmit = (event) => {
+  event.preventDefault()
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${event.target.gifName.value}`
 
+  fetch(url)
+  .then( res =>  res.json())
+  .then(data => {
+    this.setState({
+      gif: data.data
+    })
+  })
+  this.setState({
+    searchURL: url
+  })
+}
+  render() { 
+    return ( 
+<>
+<form onSubmit={this.handleSubmit}>
+          <label htmlFor='gifName'>Search: </label>
+          <input
+            id='gifName'
+            type='text'
+            // onChange={this.handleChange}
+          />
+          <input
+            type='submit'
+            value='Search a Gif'
+          />
+        </form>
+        {this.state.gif.map((gif, idx) => {
+          return(
+            <div>
+              <img src={gif.images.original.url} key={idx} alt={gif.gifName} />
+            </div>
+          )
+        })}
+</>
+     );
+  }
+}
 export default App;
